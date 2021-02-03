b3eadc4149125ca934cc76f4248711cede4e376e
Rodrigo Beker <rodrigobeker18@gmail.com>
2021-02-03 11:30:17 -0300

    Inicializando a API Rest

---
+++ src/main/java/br/com/patrimonioempresa/apiescolapatrimonioempresa/ApiEscolaPatrimonioempresaApplication.java
@@ -0,0 +1,13 @@
+package br.com.patrimonioempresa.apiescolapatrimonioempresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEscolaPatrimonioempresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEscolaPatrimonioempresaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/patrimonioempresa/apiescolapatrimonioempresa/ApiEscolaPatrimonioempresaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.patrimonioempresa.apiescolapatrimonioempresa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEscolaPatrimonioempresaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
